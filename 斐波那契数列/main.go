8b26867a00f931c8d682883f2e1d1eb0d8933dc8
蒲强 <pu.qiang@qq.com>
2023-08-31 15:05:37 +0800

    fib

--- 斐波那契数列/main.go
+++ 斐波那契数列/main.go
@@ -3,45 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(fibonacci2(3))
-}
-
-func fib(n int) int {
-	if n < 2 {
-		return n
-	}
-
-	return fib(n-1) + fib(n-2)
+	fmt.Println(fibonacci2(5))
 }
 
 func fibonacci(n int) int {
-
 	if n < 2 {
-
 		return n
-
 	}
 
 	return fibonacci(n-1) + fibonacci(n-2)
-
 }
 
 func fibonacci2(n int) int {
-
 	if n < 2 {
-
 		return n
-
 	}
 
-	n1, n2 := 0, 1
+	n1, n2 := 1, 1
 
-	for i := 2; i <= n; i++ {
-
+	for i := 3; i <= n; i++ {
 		n1, n2 = n2, n1+n2
-
 	}
 
 	return n2
-
 }
